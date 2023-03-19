8284f21d1043dbfeea28c974e0524f194c0c078b
Andrew Marek <andwmarek@gmail.com>
2023-03-19 14:56:29 -0500

    trying to fix aws example cause it no work

--- iol/src/main.rs
+++ iol/src/main.rs
@@ -1,10 +1,15 @@
+use aws_config::meta::region::RegionProviderChain;
 use lambda_runtime::{handler_fn, Context};
 use tracing::log::{error, info};
+use aws_sdk_sesv2::{Region, Client};
 use serde::{Deserialize, Serialize};
+use std::env;
+mod ses;
+use structopt::StructOpt;
+use ses::{Opt, send_message};
 
 
-/* https://docs.aws.amazon.com/sdk-for-rust/latest/dg/lambda.html */
-// step 1  -- deserailize w/ serde i guess
+
 #[derive(Deserialize)]
 struct Request {
     pub body: String,
@@ -20,15 +25,12 @@ struct FailureResponse {
     pub body: String,
 }
 
-// Implement Display for the Failure response so that we can then implement Error.
 impl std::fmt::Display for FailureResponse {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.body)
     }
 }
 
-// Implement Error for the FailureResponse so that we can `?` (try) the Response
-// returned by `lambda_runtime::run(func).await` in `fn main`.
 impl std::error::Error for FailureResponse {}
 
 type Response = Result<SuccessResponse, FailureResponse>;
@@ -41,38 +43,36 @@ async fn main() -> Result<(), lambda_runtime::Error> {
     Ok(())
 }
 
+
 async fn handler(req: Request, _ctx: lambda_runtime::Context) -> Response {
     info!("handling a request...");
-    /* 
-    let bucket_name = std::env::var("BUCKET_NAME")
-        .expect("A BUCKET_NAME must be set in this app's Lambda environment variables.");
+    let Opt {
+        contact_list,
+        region,
+        from_address,
+        message,
+        subject,
+        verbose,
+    } = Opt::from_args();
 
-    // No extra configuration is needed as long as your Lambda has
-    // the necessary permissions attached to its role.
-    let config = aws_config::load_from_env().await;
-    let s3_client = aws_sdk_s3::Client::new(&config);
-    // Generate a filename based on when the request was received.
-    let filename = format!("{}.txt", time::OffsetDateTime::now_utc().unix_timestamp());
-    let _ = s3_client
-        .put_object()
-        .bucket(bucket_name)
-        .body(req.body.as_bytes().to_owned().into())
-        .key(&filename)
-        .content_type("text/plain")
-        .send()
-        .await
-        .map_err(|err| {
-            // In case of failure, log a detailed error to CloudWatch.
-            error!(
-                "failed to upload file '{}' to S3 with error: {}",
-                &filename, err
-            );
-            // The sender of the request receives this message in response.
-            FailureResponse {
-                body: "The lambda encountered an error and your message was not saved".to_owned(),
-            }
-        })?;
+    /* gonna try to hardcode this because it's not working
+    let region_provider = RegionProviderChain::first_try(region.map(Region::new))
+        .or_default_provider()
+        .or_else(Region::new("us-west-2"));
     */
+
+    let provider = RegionProviderChain::first_try("us-east-1")
+        .or_default_provider()
+        .or_else("us-east-2");
+
+    let shared_config = aws_config::from_env().region(provider).load().await;
+
+    // TODO: try to figure out how to change types ? 
+    let client = Client::new(&shared_config);
+
+    send_message(&client, &contact_list, &from_address, &subject, &message).await;
+
+
     let filename = format!("{}.txt", time::OffsetDateTime::now_utc().unix_timestamp());
     info!(
         "YOOOOOOOOOOOOO we ran bro '{}'",
---
+++ iol/src/ses.rs
@@ -0,0 +1,78 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+ use aws_sdk_sesv2::model::{Body, Content, Destination, EmailContent, Message};
+ use aws_sdk_sesv2::{Client, Error, Region, PKG_VERSION};
+ use structopt::StructOpt;
+ 
+ #[derive(Debug, StructOpt)]
+pub struct Opt {
+     #[structopt(short, long)]
+     contact_list: String,
+ 
+     #[structopt(short, long)]
+     region: Option<String>,
+ 
+     #[structopt(short, long)]
+     from_address: String,
+ 
+     /// The message of the email.
+     #[structopt(short, long)]
+     message: String,
+ 
+     #[structopt(short, long)]
+     subject: String,
+ 
+     /// Whether to display additional information.
+     #[structopt(short, long)]
+     verbose: bool,
+ }
+ 
+ // Sends a message to all members of the contact list.
+ // snippet-start:[ses.rust.send-email]
+ pub async fn send_message(
+     client: &Client,
+     list: &str,
+     from: &str,
+     subject: &str,
+     message: &str,
+ ) -> Result<(), Error> {
+     // Get list of email addresses from contact list.
+     let resp = client
+         .list_contacts()
+         .contact_list_name(list)
+         .send()
+         .await?;
+ 
+     let contacts = resp.contacts().unwrap_or_default();
+ 
+     let cs: String = contacts
+         .iter()
+         .map(|i| i.email_address().unwrap_or_default())
+         .collect();
+ 
+     let dest = Destination::builder().to_addresses(cs).build();
+     let subject_content = Content::builder().data(subject).charset("UTF-8").build();
+     let body_content = Content::builder().data(message).charset("UTF-8").build();
+     let body = Body::builder().text(body_content).build();
+ 
+     let msg = Message::builder()
+         .subject(subject_content)
+         .body(body)
+         .build();
+ 
+     let email_content = EmailContent::builder().simple(msg).build();
+ 
+     client
+         .send_email()
+         .from_email_address(from)
+         .destination(dest)
+         .content(email_content)
+         .send()
+         .await?;
+ 
+     println!("Email sent to list");
+ 
+     Ok(())
+ } 
